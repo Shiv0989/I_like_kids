d1dabcd1c343df13f462e47da2310d0096cb6333
Shiv0989 <83575973+Shiv0989@users.noreply.github.com>
2021-10-09 22:38:16 +0530

    Update COVID-19 liberator.py

--- COVID-19 liberator.py
+++ COVID-19 liberator.py
@@ -1,120 +1,136 @@
-
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
 import numpy as np
-import cv2
-import imutils
 import os
-import time
 
-def Check(a,  b):
-    dist = ((a[0] - b[0]) ** 2 + 550 / ((a[1] + b[1]) / 2) * (a[1] - b[1]) ** 2) ** 0.5
-    calibration = (a[1] + b[1]) / 2       
-    if 0 < dist < 0.25 * calibration:
-        return True
-    else:
-        return False
+# initialize the initial learning rate, number of epochs to train for,
+# and batch size
+INIT_LR = 1e-4
+EPOCHS = 20
+BS = 32
 
-def Setup(yolo):
-    global net, ln, LABELS
-    weights = os.path.sep.join([yolo, "yolov3.weights"])
-    config = os.path.sep.join([yolo, "yolov3.cfg"])
-    labelsPath = os.path.sep.join([yolo, "coco.names"])
-    LABELS = open(labelsPath).read().strip().split("\n")  
-    net = cv2.dnn.readNetFromDarknet(config, weights)
-    ln = net.getLayerNames()
-    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+DIRECTORY = r"C:\Mask Detection\CODE\Face-Mask-Detection-master\dataset"
+CATEGORIES = ["with_mask", "without_mask"]
 
-def ImageProcess(image):
-    global processedImg
-    (H, W) = (None, None)
-    frame = image.copy()
-    if W is None or H is None:
-        (H, W) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-    net.setInput(blob)
-    starttime = time.time()
-    layerOutputs = net.forward(ln)
-    stoptime = time.time()
-    print("Video is Getting Processed at {:.4f} seconds per frame".format((stoptime-starttime))) 
-    confidences = []
-    outline = []
-    
-    for output in layerOutputs:
-        for detection in output:
-            scores = detection[5:]
-            maxi_class = np.argmax(scores)
-            confidence = scores[maxi_class]
-            if LABELS[maxi_class] == "person":
-                if confidence > 0.5:
-                    box = detection[0:4] * np.array([W, H, W, H])
-                    (centerX, centerY, width, height) = box.astype("int")
-                    x = int(centerX - (width / 2))
-                    y = int(centerY - (height / 2))
-                    outline.append([x, y, int(width), int(height)])
-                    confidences.append(float(confidence))
+# grab the list of images in our dataset directory, then initialize
+# the list of data (i.e., images) and class images
+print("[INFO] loading images...")
 
-    box_line = cv2.dnn.NMSBoxes(outline, confidences, 0.5, 0.3)
+data = []
+labels = []
 
-    if len(box_line) > 0:
-        flat_box = box_line.flatten()
-        pairs = []
-        center = []
-        status = [] 
-        for i in flat_box:
-            (x, y) = (outline[i][0], outline[i][1])
-            (w, h) = (outline[i][2], outline[i][3])
-            center.append([int(x + w / 2), int(y + h / 2)])
-            status.append(False)
+for category in CATEGORIES:
+    path = os.path.join(DIRECTORY, category)
+    for img in os.listdir(path):
+    	img_path = os.path.join(path, img)
+    	image = load_img(img_path, target_size=(224, 224))
+    	image = img_to_array(image)
+    	image = preprocess_input(image)
 
-        for i in range(len(center)):
-            for j in range(len(center)):
-                close = Check(center[i], center[j])
+    	data.append(image)
+    	labels.append(category)
 
-                if close:
-                    pairs.append([center[i], center[j]])
-                    status[i] = True
-                    status[j] = True
-        index = 0
+# perform one-hot encoding on the labels
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
 
-        for i in flat_box:
-            (x, y) = (outline[i][0], outline[i][1])
-            (w, h) = (outline[i][2], outline[i][3])
-            if status[index] == True:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 150), 2)
-            elif status[index] == False:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            index += 1
-        for h in pairs:
-            cv2.line(frame, tuple(h[0]), tuple(h[1]), (0, 0, 255), 2)
-    processedImg = frame.copy()
-            
-create = None
-frameno = 0
-filename = "videos/video_6.mp4"
-yolo = "yolo-coco/"
-opname = "output_videos/output_of_" + filename.split('/')[1][:-4] + '.mp4'
-cap = cv2.VideoCapture(filename)
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
 
-time1 = time.time()
-while(True):
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
 
-    ret, frame = cap.read()
-    if not ret:
-        break
-    current_img = frame.copy()
-    current_img = imutils.resize(current_img, width=480)
-    video = current_img.shape
-    frameno += 1
+# construct the training image generator for data augmentation
+aug = ImageDataGenerator(
+	rotation_range=20,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=True,
+	fill_mode="nearest")
 
-    if(frameno%2 == 0 or frameno == 1):
-        Setup(yolo)
-        ImageProcess(current_img)
-        Frame = processedImg
+# load the MobileNetV2 network, ensuring the head FC layer sets are
+# left off
+baseModel = MobileNetV2(weights="imagenet", include_top=False,
+	input_tensor=Input(shape=(224, 224, 3)))
 
-        if create is None:
-            fourcc = cv2.VideoWriter_fourcc(*'XVID')
-            create = cv2.VideoWriter(opname, fourcc, 30, (Frame.shape[1], Frame.shape[0]), True)
-    create.write(Frame)
-    if cv2.waitKey(1) & 0xFF == ord('s'):
-        break
-time2 = time.time()
-print("Completed. Total Time Taken: {} minutes".format((time2-time1)/60))
+# construct the head of the model that will be placed on top of the
+# the base model
+headModel = baseModel.output
+headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(128, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
+
+# place the head FC model on top of the base model (this will become
+# the actual model we will train)
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+# loop over all layers in the base model and freeze them so they will
+# *not* be updated during the first training process
+for layer in baseModel.layers:
+	layer.trainable = False
+
+# compile our model
+print("[INFO] compiling model...")
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+# train the head of the network
+print("[INFO] training head...")
+H = model.fit(
+	aug.flow(trainX, trainY, batch_size=BS),
+	steps_per_epoch=len(trainX) // BS,
+	validation_data=(testX, testY),
+	validation_steps=len(testX) // BS,
+	epochs=EPOCHS)
+
+# make predictions on the testing set
+print("[INFO] evaluating network...")
+predIdxs = model.predict(testX, batch_size=BS)
+
+# for each image in the testing set we need to find the index of the
+# label with corresponding largest predicted probability
+predIdxs = np.argmax(predIdxs, axis=1)
+
+# show a nicely formatted classification report
+print(classification_report(testY.argmax(axis=1), predIdxs,
+	target_names=lb.classes_))
+
+# serialize the model to disk
+print("[INFO] saving mask detector model...")
+model.save("mask_detector.model", save_format="h5")
+
+# plot the training loss and accuracy
+N = EPOCHS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig("plot.png")
